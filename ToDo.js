571f40a6d9e177b30a03ff8bd98f3069a45ec13a
Chrisql <81173766+Chrisql@users.noreply.github.com>
2022-02-01 16:40:14 -0600

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -1,2 +1,14 @@
-let buttons = document.querySelector('.buttons');
-// I dont know what else to do next
+//Calling the var(s) from html
+let incomingStuff = document.querySelector('.input');
+let submitButton = document.querySelector('.button');
+let clearButton = document.querySelector('.clearButton');
+let list = document.querySelector('.theList');
+
+//Listening for the button click
+submitButton.addEventListener('click', submit);
+clearButton.addEventListener('click', clearList);
+
+//thinking about how im going to create a new element for the list
+function newElemnt(){
+
+}
